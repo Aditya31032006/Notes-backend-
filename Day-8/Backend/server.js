8ee200facbb816d19c2fd807bfc0a6c0f6879337
Aditya N. Gandhi <adityangandhi@gmail.com>
2026-03-12 20:25:33 +0530

    upgradation integration

--- Day-8/Backend/server.js
+++ Day-8/Backend/server.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const app=require("./src/app")
 
 const connectToData= require("./src/config/Database")
-connectToData
+connectToData()
 
 
 
--- Day-8/Backend/src/app.js
+++ Day-8/Backend/src/app.js
@@ -7,8 +7,7 @@ const path =require("path")
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static("./public"))
-
+app.use(express.static(path.join(__dirname,"public")))
 app.post("/api/notes", async(req,res)=>{
     const {title,description}=req.body
 
